9b40f353efb0a4f2daf2abc32fe5ed4bf81ece67
Maciej Tyski <maciej.tyski@pl.abb.com>
2022-05-30 11:52:04 +0200

    feat: add compose support for local tests

--- src/Minimal.Api/Program.cs
+++ src/Minimal.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,9 @@ using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.ConfigureAppConfiguration(
+    static (_, config) => config.AddEnvironmentVariables());
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,7 +25,8 @@ builder.Services.AddDbContext<TodoContext>(
         Host = builder.Configuration["Database:Host"],
         Database = builder.Configuration["Database:Source"],
         Username = builder.Configuration["Database:User"],
-        Password = builder.Configuration["Database:Password"]
+        Password = builder.Configuration["Database:Password"],
+        Port = int.Parse(builder.Configuration["Database:Port"])
     }.ToString()));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -33,7 +38,7 @@ builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPip
 builder.Services.AddValidatorsFromAssemblyContaining<Delete.Request.Validator>();
 
 builder.Services.Configure<JsonOptions>(
-    static opts => opts.SerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+    static opts => opts.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)));
 
 var app = builder.Build();
 
